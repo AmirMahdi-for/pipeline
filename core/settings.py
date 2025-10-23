d260359c47557235ee8555a7631c52a3dd5d9e7f
AmirMahdi-for <amirmahdifor@gmail.com>
2025-10-23 23:39:59 +0330

    fix: set static file in settings

--- core/settings.py
+++ core/settings.py
@@ -146,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
